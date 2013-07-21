e3a6e35f7f47e15ed4e83093dcd90a109fba7d65
Peter Trško <peter.trsko@gmail.com>
2013-07-21 20:19:21 +0200

    Dropping MonadExceptionUtilities constraint

--- src/System/IO/LockFile/Resource.hs
+++ src/System/IO/LockFile/Resource.hs
@@ -27,13 +27,12 @@ module System.IO.LockFile.Resource
 import Control.Applicative ((<$>))
 
 import Control.Monad.TaggedException (hide)
-#if MIN_VERSION_tagged_exception_core(1,1,0)
-import Control.Monad.TaggedException (MonadExceptionUtilities)
-#else
-import Control.Monad.TaggedException.Utilities (MonadExceptionUtilities)
-#endif
 
 import Control.Monad.Trans.Resource
+    ( MonadResource(liftResourceT)
+    , ReleaseKey
+    , allocate
+    )
 import System.IO.LockFile.Internal
     ( LockingException(..)
     , LockingParameters(..)
@@ -44,11 +43,12 @@ import System.IO.LockFile.Internal
 
 
 acquireLockFile
-    :: (MonadExceptionUtilities m, MonadResource m)
+    :: MonadResource m
     => LockingParameters
     -> FilePath
     -> m ReleaseKey
-acquireLockFile params lockFileName = fst <$> allocate lock' unlock'
+acquireLockFile params lockFileName =
+    liftResourceT $ fst <$> allocate lock' unlock'
   where
     lock' = hide $ lock params lockFileName
     unlock' = hide . unlock lockFileName
